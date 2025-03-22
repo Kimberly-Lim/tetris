da6e2c51e8cdf006b1743c373fd578e2202b56b3
kimberly <knlim@crimson.ua.edu>
2025-03-22 16:09:27 -0500

    updated all code, gameplay works

--- react-tetris/src/components/Cell.js
+++ react-tetris/src/components/Cell.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import { StyledCell } from './styles/StyledCell';
+import { TETROMINOS } from '../tetrominos';
 
+// React.memo makes sure we only re-render the changed cells
 const Cell = ({ type }) => (
-    <div>cell</div>
-)
+  <StyledCell type={type} color={TETROMINOS[type].color}>
+    {console.log('rerender cell')}
+  </StyledCell>
+);
 
-export default Cell;
+export default React.memo(Cell);
